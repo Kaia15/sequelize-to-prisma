40bed83e92ade612168e8aecdd57cd41819378e3
Tran Ong <baotranongtran@gmail.com>
2023-04-18 04:04:53 -0400

    041823

--- routes/companies.js
+++ routes/companies.js
@@ -1,7 +1,8 @@
 const Company = require('../models/company.model')
 const express = require('express');
 const router = express.Router();
-const {Op} = require('sequelize')
+const {Op} = require('sequelize');
+const sequelize = require('../config/database');
 
 router.get('/', async function (req, res, next) {
     // const new_company = await Company.create({
@@ -10,30 +11,44 @@ router.get('/', async function (req, res, next) {
     //     funding: 4000000,
     //     private: true
     // })
-    const companies = await Company.findAll()
-    await res.send(companies)
-    // await Company.update({profit: 0, type: 'startup'}, {
-    //     where: {
-    //         [Op.and]: [
-    //             { id: 1}, {profit: null}, {type: null}
-    //         ]
-    //     }
+    // const companies = await Company.findAll({
+    //     attributes: ['id', 'name', 'profit']
     // })
+
+    // const triplea = await Company.findOne({where: {id: 1}})
+    // triplea.profit = 0
+    // await triplea.update({profit: 1000000})
+    // await triplea.save()
+    // triplea.profit = 1000000
+    // await triplea.save()
+
+    // const companies = await Company.findAll({
+    //     attributes: ['id', 'name', 'profit']
+    // })
+    // await res.send(companies)
+
+    // ***
+    // await sequelize.query('UPDATE "Companies" SET profit = 1000000 WHERE id = 1')
+
+
+    try {
+        const t = await sequelize.transaction();
+        const triplea = await Company.findOne({where: {id: 1}})
+        await triplea.update({profit: 1200000})
+        await triplea.save()
+        await t.commit()
+
+        const companies = await Company.findAll({
+            attributes: ['profit']
+        })
+        await res.send(companies)
+    } catch(err) {
+        console.log(err)
+    }
+    
 })
 
 router.get('/update', async function (req,res,next) {
-    // await Company.update({profit: 0, type: 'startup'}, {
-    //     where: {
-    //         [Op.and]: [
-    //             { id: 1}, {profit: null}, {type: null}
-    //         ]
-    //     }
-    // })
-    await Company.update({profit: 0}, {
-        where: {
-            profit: null
-        }
-    })
     await res.render('index', { title: 'success!' });
 })
 
